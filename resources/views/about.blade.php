ead2c6e97a0da17717bde713c80c8b02d787d3f7
safafadillah13 <safafadillah@smkwikrama.sch.id>
2024-06-13 15:38:45 +0700

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Safa Fadillah-Web</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <!--box icons link-->
+    <link rel="stylesheet"
+    href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+    <!--remix icons link-->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+
+    <!--Google fonts link-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@300;400;500;600;700;800;900&family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+ </head>
+<body>
+
+
+    <!--header-->
+    <header>
+        <a href="#" class="logo">Developer <span>X.</span></a>
+        <div class="bx bx-menu" id="menu-icon"></div>
+
+        <ul class="navlist">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#Skill">Skill</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div class="top-btnn">
+            <a href="https://wa.me/085172097636" class="h-btn">Contact me</a>
+        </div>
+    </header>
+
+    <!--hero-->
+    <section class="hero" id="home">
+        <div class="main-content" data-aos="fade-in">
+            <h4>Hi, There!</h4>
+            <h1>I'm <span>Safa Fadillah</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae veniam dignissimos saepe esse dolorem id. Aliquid maiores reiciendis distinctio commodi praesentium, eos quod fugiat vitae ab? Tempore eaque iure accusamus.</p>
+            <div class="social">
+                <a href="https://wa.me/085172097636"><i class="ri-whatsapp-fill"></i></a>
+                <a href="https://www.instagram.com/safaa.lyandra/"><i class="ri-instagram-fill"></i></a>
+                <a href="https://github.com/Safafadillah"><i class="ri-github-fill"></i></a>
+                <a href="https://www.youtube.com/channel/UCucOnRuoYVAq0jkoZOEkyIA"><i class="ri-youtube-fill"></i></a>
+            </div>
+
+            <div class="main-btn">
+                <a href="https://drive.google.com/file/d/15lb_iM7r-27bfcdYF1VCda6qYKNmkjcB/view" class="btn">Hire me</a>
+                <a href="#" class="btn btn2">Dowload CV</a>
+            </div>
+        </div>
+    </section>
+    <section class="about" id="about">
+        <div class="about-img" data-aos= "zoom-in-down">
+            <img src="img/apadehh.jpeg" alt="about">
+        </div>
+        <div class="about-text" data-aos= "zoom-in-up">
+            <h2> My Name <span> Safa Fadillah</span></h2>
+            <div class="exp-area">
+                <p class="exp">
+                    I was born in Bogor and now I am 16 years old, I am the fourth of four children, I study at Wikrama Vocational High School, my hobbies are playing futsal, coding and listening to music, my dream is to become a full stack developer, that's all from me
+                    <span></span>
+                </p>
+            </div>
+            <a href="https://github.com/Safafadillah" class="btn">View All Project</a>
+        </div>
+    </section>
+
+    <section class="services" id="Skill">
+        <div class="center-text" data-aos= "fade-down">
+            <h2>My <span>Skill</span></h2>
+        </div>
+
+        <div class="skill-content" data-aos= "zoom-in-up">
+            <div class="box">
+                <img src="img/800px-HTML5_Badge.svg.png" alt="i" width="50px" height="50px"> 
+                <h3>HTML (HyperText Markup Language)</h3>
+                <p>fringlla dapibus pleatan interdum justoutan
+                elentum lectus tellus midigin issimer motana egestas
+                imperdiet solution</p>
+                <a href="https://wiggly-library-a46.notion.site/Instalasi-Visual-Studio-Code-dan-Membuat-File-HTML-8d923981abf146e0a6b34a97f8d2e74d">
+                    MORE
+                    <i class="ri-arrow-right-line"></i>
+                </a>
+            </div>
+            <div class="box">
+                <img src="img/1.png" alt="i" width="50px" height="50px"> 
+                <h3>CSS (Cascanding Style Sheet)</h3>
+                <p>fringlla dapibus pleatan interdum justoutan
+                elentum lectus tellus midigin issimer motana egestas
+                imperdiet solution</p>
+                <a href="https://wiggly-library-a46.notion.site/CSS-Basic-088515bbd52d4b769412a578014c8f0f">
+                    MORE
+                    <i class="ri-arrow-right-line"></i>
+                </a>
+            </div>
+            <div class="box">
+                <img src="img/js2.png" alt="i" width="50px" height="50px"> 
+                <h3>JS (Java Script)</h3>
+                <p>fringlla dapibus pleatan interdum justoutan
+                elentum lectus tellus midigin issimer motana egestas
+                imperdiet solution</p>
+                <a href="https://wiggly-library-a46.notion.site/Javascript-f567ebac3e86450ca6a7668b96ff903f">
+                    MORE
+                    <i class="ri-arrow-right-line"></i>
+                </a>
+            </div>
+            <div class="box">
+                <img src="img/php.png" alt="i" width="50px" height="50px"> 
+                <h3>PHP (Hypertext Preprocessor)</h3>
+                <p>fringlla dapibus pleatan interdum justoutan
+                elentum lectus tellus midigin issimer motana egestas
+                imperdiet solution</p>
+                <a href="https://wiggly-library-a46.notion.site/Introduction-to-PhP-56cfed51d17a47b392a5fc79515462d3">
+                    MORE
+                    <i class="ri-arrow-right-line"></i>
+                </a>
+            </div>
+            <div class="box">
+                <img src="img/o.png" alt="OOP" width="50px" height="50px"> 
+                <h3>OOP (Object Oriented Programing)</h3>
+                <p>fringlla dapibus pleatan interdum justoutan
+                elentum lectus tellus midigin issimer motana egestas
+                imperdiet solution</p>
+                <a href="https://wiggly-library-a46.notion.site/OOP-Object-Oriented-Programming-4ec3b192b58642e2b38d7587a183dc2e">
+                    MORE
+                    <i class="ri-arrow-right-line"></i>
+                </a>
+            </div>
+            <div class="box">
+                <img src="img/6.png" alt="i" width="50px" height="50px"> 
+                <h3>Laravel</h3>
+                <p>fringlla dapibus pleatan interdum justoutan
+                elentum lectus tellus midigin issimer motana egestas
+                imperdiet solution</p>
+                <a href="https://wiggly-library-a46.notion.site/Framework-Laravel-X-fc8b042b557e46ffa6ab0896c3dd32c9">
+                    MORE
+                    <i class="ri-arrow-right-line"></i>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <section class="Contact" id="contact">
+        <div class="center-text" data-aos= "fade-down">
+            <h2>Contact <span> Me</span></h2>
+        </div>
+
+        <div class="contact-form" data-aos= "zoom-in">
+            <form action="">
+                <input type="text" placeholder="Your name" required>
+                <input type="email" placeholder="email Address.." required>
+                <textarea name="" id="" cols="30" rows="10" placeholder="Write Message Here..." required></textarea>
+                <input type="submit" name="" value="Send Message" class="send-btn">
+            </form>
+        </div>
+
+        <!-- <div>
+            safa
+            <img src="img/safa.jpg" alt="safa" srcset="">
+        </div> -->
+
+    </section>
+    <div class="footer">
+        <div class="copyright">
+            <p>Safa Fadillah 2023</p>
+        </div>
+        <a href="#home" class="scroll-top">
+            <i class="ri-arrow-up-s-fill"></i>
+        </a>
+    </div>
+    <script src="js/script.js"></script> 
+
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+    AOS.init({
+        offset: 300,
+        duration: 1400,
+    });
+    </script>
+</body>
+</html>
